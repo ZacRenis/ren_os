9de97183935103725bb5888f1e2b926aa90db562
ZacRenis <self@zhichao.ren>
2019-05-25 11:22:04 +0800

    cannot run the print_something fuction

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,9 @@
 
 use core::panic::PanicInfo;
 
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
+mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!Hello Zac.R!";
+static HELLO: &[u8] = b"Hello Zac.R!";
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
@@ -23,5 +19,11 @@ pub extern "C" fn _start() -> ! {
     }
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
